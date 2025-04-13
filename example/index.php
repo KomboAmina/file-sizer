5578db29bb6a83688be3bb37118d858892a3f83d
Amina Kombo <68789213+KomboAmina@users.noreply.github.com>
2025-04-13 17:48:21 +0300

    Add files via upload

---
+++ example/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * create filesizer static object
+ * use object to find size of file in bits/bytes/kb,mb,gb,etc
+ * use object to convert filesize to another size
+ */
+
+ require_once "/path/to/FileSizer.php";
+
+ $fileSize = FileSizer::getFileSize("file.txt","K");
+
+ print_r($fileSize);
+
+ echo "<hr />";
+
+ echo "<p>".number_format($fileSize['size'],2)." ".$fileSize['longunit']."</p>";
+
